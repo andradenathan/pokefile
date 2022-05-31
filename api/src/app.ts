d3c8de72eb5655c8d95c199542e9491fa6257e4d
Nathan Andrade <nathan.nhdsk@gmail.com>
2022-05-31 14:47:05 -0300

    [02] implementa user-repository e crud

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import 'reflect-metadata';
 import 'dotenv/config';
-import routes from "./routes/routes";
-const app = express();
+
+import { Container } from 'inversify';
+import { InversifyExpressServer } from 'inversify-express-utils';
+import { UserRepository } from "./app/user/user.repository";
+import "./app/user/user.controller";
+
+const container = new Container();
+container.bind(UserRepository).toSelf();
+
+const server = new InversifyExpressServer(container);
+
+const app = server.build();
 
 app.use(express.json());
-app.use(routes);
 
 app.listen(process.env.APP_PORT);
