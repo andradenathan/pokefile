3b7c9227aa0d63371ea6223bab37c3dfb72111f6
Nathan Andrade <nathan.nhdsk@gmail.com>
2022-05-31 18:14:40 -0300

    [03] refatoracao de codigo

--- api/src/app/pokemon/pokemon.controller.ts
+++ api/src/app/pokemon/pokemon.controller.ts
@@ -1,11 +1,7 @@
-import { Pokemon } from "@prisma/client";
-
 import { 
     Request, 
     Response 
 } from "express";
-import { injectable } from "inversify";
-
 import { 
     controller, 
     httpDelete, 
@@ -14,11 +10,15 @@ import {
     httpPut 
 } from "inversify-express-utils";
 import { PokemonRepository } from "./pokemon.repository";
+import PokemonService from "./pokemon.service";
 
 
 @controller('/pokemons')
 export default class PokemonController {
-    constructor(private readonly pokemonRepository: PokemonRepository) {}
+    constructor(
+        private readonly pokemonService: PokemonService,
+        private readonly pokemonRepository: PokemonRepository
+    ) {}
     
     @httpGet('/')
     async all(request: Request, response: Response) {
@@ -33,7 +33,7 @@ export default class PokemonController {
     @httpGet('/populate')
     async populate(request: Request, response: Response) {
         try {
-            const pokemons = await this.pokemonRepository.getAllPokemonsAndStore();
+            const pokemons = await this.pokemonService.execute();
             return response.status(200).json({success: {pokemons: pokemons}});
         } catch(err: any) {
             return response.status(422).json({error: {message: err.message}});
--- api/src/app/pokemon/pokemon.repository.ts
+++ api/src/app/pokemon/pokemon.repository.ts
@@ -5,33 +5,6 @@ import prismaClient from "../../database/prisma";
 
 import PokemonService from "./pokemon.service";
 
-interface PokemonResponseData {
-    name: string;
-    weight: number;
-    height: number;
-    sprites: {
-        back_default: string;
-        back_female: string;
-        back_shiny: string;
-        back_shiny_female: string;
-        front_default: string;
-        front_female: string;
-        front_shiny: string;
-        front_shiny_female: string;
-    }
-    stats: Array<{
-        base_stat: number;
-        stat: {
-            name: string;
-        }
-    }>
-    types: Array<{
-        type: {
-            name: string;
-        }
-    }>
-}
-
 @injectable()
 export class PokemonRepository {
     constructor(private readonly pokemonService: PokemonService) {}
@@ -49,81 +22,8 @@ export class PokemonRepository {
         return await prismaClient.pokemon.findUnique({where: {id: id}});
     }
     
-    async getAllPokemonsAndStore(): Promise<string> {
-        const FIRST_SEASON_LAST_POKEMON_ID = 152;
-
-        for(let i = 1; i < FIRST_SEASON_LAST_POKEMON_ID; i++) {
-            const { data }: AxiosResponse<PokemonResponseData> 
-            = await this.pokemonService.getAllPokemons(i);                                    
-        
-            const pokemonData = {
-                name: data.name,
-                weight: data.weight,
-                height: data.height,
-                baseAttack: 0,
-                baseDefense: 0,
-                baseSpecialAttack: 0,
-                baseSpecialDefense: 0,
-                baseSpeed: 0,
-                baseHp: 0,
-            }
-
-            data.stats.forEach((status) => {
-                switch(status.stat.name) {
-                    case 'hp':
-                        pokemonData['baseHp'] = status.base_stat;
-                        break;
-                    case 'attack':
-                        pokemonData['baseAttack'] = status.base_stat;
-                        break;
-                    case 'defense':
-                        pokemonData['baseDefense'] = status.base_stat;
-                        break;
-                    case 'special-attack':
-                        pokemonData['baseSpecialAttack'] = status.base_stat;
-                        break;
-                    case 'special-defense':
-                        pokemonData['baseSpecialDefense'] = status.base_stat;
-                        break;
-                    case 'speed':
-                        pokemonData['baseSpeed'] = status.base_stat;
-                        break;
-                }
-            });
-
-            await prismaClient.pokemon.create({data: pokemonData});
-
-            data.types.forEach(async(type) => {
-                const typesData = {
-                    name: type.type.name,
-                    pokemonName: data.name,
-                }
-
-                await prismaClient.type.create({data: typesData});
-            });
-
-            for(let image of Object.entries(data.sprites)) {
-                if(image[1] != null && typeof image[1] === 'string') {
-                    if(image[1].endsWith('.png')) {
-                        const imagesData = {
-                            path: image[1],
-                            pokemonId: i,
-                        }
     
-                        await prismaClient.image.create({data: imagesData});
-    
-                    } else {
-                        continue;
-                    }
-                } else {
-                    continue;
-                }
-            }
-        }
 
-        return "Pokémons registrados com sucesso!";
-    }
-    
     async update(id: number, data: Prisma.PokemonUpdateInput): Promise<Pokemon> {
         return await prismaClient.pokemon.update(
             {
@@ -136,5 +36,4 @@ export class PokemonRepository {
     async delete(id: number): Promise<void> {
         await prismaClient.pokemon.delete({where: {id: id}});
     }
-    
 }
